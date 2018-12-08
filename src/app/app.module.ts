c374c552efb9faa8d24024f1b68362cdaefc0aae
vietcoder1998 <duyviet2841998@gmail.com>
2018-12-08 07:53:24 +0700

    lock-love

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import {MetaModule} from './meta/meta.module';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatToolbarModule
+} from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { ViewComponent } from './view/view.component';
+import { PromiseComponent } from './promise/promise.component';
+import { CommonComponent } from './common/common.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ViewComponent,
+    PromiseComponent,
+    CommonComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MetaModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
